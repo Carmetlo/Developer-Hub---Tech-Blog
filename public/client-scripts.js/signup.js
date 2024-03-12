462d51ad6678407c24411b0d31e5a00999b21749
Michael Carmelo <michaelcarmelo22@gmail.com>
2024-03-12 03:36:09 -0600

    finished up sighnup.js adding handlers

--- public/client-scripts.js/signup.js
+++ public/client-scripts.js/signup.js
@@ -19,3 +19,28 @@ const signupFormHandler = async (event) => {
         }
     }
 }
+
+const loginFormHandler = async (event) => {
+    event.preventDefault();
+  
+    const email = document.querySelector('#email-login').value.trim();
+    const password = document.querySelector('#password-login').value.trim();
+    console.log(email && password);
+
+    if (email && password) {
+        const response = await fetch ('api.users/login', {
+            method: 'POST',
+            body: JSON.stringify({email, password}),
+            headers: {'Content-Type': 'application/json'},
+        });
+        if (response.ok) {
+            document.location.replace('/dashboard');
+        }
+        else {
+            alert('Failed to log in');
+        }
+    }
+}
+
+document.querySelector('.signup-form').addEventListener('submit', signupFormHandler);
+document.querySelector('.login-form').addEventListener('submit', loginFormHandler)
